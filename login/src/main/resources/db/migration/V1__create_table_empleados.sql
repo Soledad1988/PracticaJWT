469be35819dd749acf9e9ff742c8d3767e0bc729
Brenda <brentorres26@gmail.com>
2023-06-10 20:24:50 -0300

    migration con Flyway

---
+++ login/src/main/resources/db/migration/V1__create_table_empleados.sql
@@ -0,0 +1,12 @@
+CREATE TABLE empleados(
+id int not null auto_increment,
+nombre varchar(100) not null,
+apellido varchar(200) not null,
+edad varchar(20),
+dni varchar(50),
+telefono varchar(50) not null,
+direccion varchar(150),
+
+
+primary key(id)
+);
